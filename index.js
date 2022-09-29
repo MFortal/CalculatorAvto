7da09017670b01fb285574d5db710e13c99a0a00
Polyakova Maria <polyakovamy@yandex.ru>
2022-09-29 21:40:54 +0300

    Подчет всех характеристик

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-let inputRight = document.getElementById("input-right"),
-  thumb = document.querySelector(".slider__thumb"),
-  range = document.querySelector(".slider__range"),
-  outputSymbol = document.querySelector(".output__symbol"),
-  output = document.querySelector(".output"),
-  slider = document.querySelector(".container-slider");
+const inputs = Array.from(document.querySelectorAll(".input-slider")),
+  thumbs = Array.from(document.querySelectorAll(".slider__thumb")),
+  ranges = Array.from(document.querySelectorAll(".slider__range")),
+  outputSymbols = Array.from(document.querySelectorAll(".output__symbol")),
+  outputs = Array.from(document.querySelectorAll(".output")),
+  sliders = Array.from(document.querySelectorAll(".container-slider")),
+  price = document.querySelector(".input-priceAvto"),
+  leasingAmount = document.querySelector(".output__leasingAmount"),
+  monthPay = document.querySelector(".output__monthPay");
 
 /*Разделение числа и добавление пробела*/
-var thousandSeparator = function (str) {
+const thousandSeparator = (str) => {
   var parts = (str + '').split('.'),
     main = parts[0],
     len = main.length,
@@ -28,40 +31,78 @@ var thousandSeparator = function (str) {
 };
 
 /* Установление значений в ползунок */
-function setRightValue() {
-  var _this = inputRight,
+const setValues = (input, i) => {
+  var _this = input,
     min = parseInt(_this.min),
     max = parseInt(_this.max);
   _this.value = parseInt(_this.value);
   var percent = ((_this.value - min) / (max - min)) * 100;
-  outputSymbol.innerHTML = thousandSeparator(Math.round(_this.value));
-  thumb.style.right = (100 - percent) + "%";
-  range.style.right = (100 - percent) + "%";
+
+  // Если перемещен ползунок первоначального взноса
+  if (outputSymbols[i].dataset.persent) {
+    console.log('value = ' + _this.value);
+    console.log('value/100 = ' + _this.value / 100);
+    console.log('price avto =  ' + price.value);
+    console.log('total = ' + _this.value / 100 * price.value);
+    outputSymbols[i].innerHTML = thousandSeparator(Math.round(_this.value / 100 * price.value));
+    document.querySelector(".output__persent").innerHTML = _this.value + '%';
+  } else {
+    outputSymbols[i].innerHTML = thousandSeparator(Math.round(_this.value));
+  }
+
+  // Перемещение ползунка
+  thumbs[i].style.right = (100 - percent) + "%";
+  ranges[i].style.right = (100 - percent) + "%";
+
+  calculation();
 }
-setRightValue();
 
-inputRight.addEventListener("input", setRightValue);
+/* Подсчет значений */
+const calculation = () => {
+  /* Ежемесячный платеж 
+  (Стоимость автомобиля inputs[0] - Первоначальный взнос) * ((Процентная ставка * (1 + Процентная ставка) ^ Срок кредита в месяцах) / ((1 + Процентная ставка) ^ Срок кредита в месяцах - 1))
+  const monthPay = (price - initial) * ((0.035 * Math.pow((1 + 0.035), months)) / (Math.pow((1 + 0.035), months) - 1));
+  */
+  const mPay = Math.round((inputs[0].value - inputs[1].value) * ((0.035) * Math.pow((1 + 0.035), inputs[2].value)) / (Math.pow((1 + 0.035), inputs[2].value) - 1));
+  monthPay.innerHTML = thousandSeparator(mPay) + ' ₽';
 
-inputRight.addEventListener("mouseover", function () {
-  thumb.classList.add("slider__thumb_hover");
-  range.classList.add("slider__range_hover");
+  /* Сумма договора лизинга
+  Первоначальный взнос + Срок кредита в месяцах * Ежемесячный платеж
+  */
+  leasingAmount.innerHTML = thousandSeparator(Math.round((inputs[1].value / 100 * price.value) + mPay * inputs[2].value)) + ' ₽';
+}
 
-  /* Состояние Active */
-  range.classList.add("slider__range_active");
-  output.classList.add("output_active");
-});
+inputs.forEach((item, i) => {
+  setValues(item, i);
+  item.addEventListener("input", () => {
+    setValues(item, i);
+  });
+})
 
-inputRight.addEventListener("mouseout", function () {
-  thumb.classList.remove("slider__thumb_hover");
-  range.classList.remove("slider__range_hover");
+inputs.forEach((item, i) => {
+  item.addEventListener("mouseover", function () {
+    thumbs[i].classList.add("slider__thumb_hover");
+    ranges[i].classList.add("slider__range_hover");
 
-  /* Состояние Active */
-  range.classList.remove("slider__range_active");
-  output.classList.remove("output_active");
-});
+    /* Состояние Active */
+    ranges[i].classList.add("slider__range_active");
+    outputs[i].classList.add("output_active");
+  });
+
+  item.addEventListener("mouseout", function () {
+    thumbs[i].classList.remove("slider__thumb_hover");
+    ranges[i].classList.remove("slider__range_hover");
+
+    /* Состояние Active */
+    ranges[i].classList.remove("slider__range_active");
+    outputs[i].classList.remove("output_active");
+  });
+})
 
 /* Состояние disabled, если data-disabled=true */
-if (slider.dataset.disabled) {
-  slider.classList.add("container-slider_disabled");
-  inputRight.disabled = "true";
-}
+sliders.forEach((item, i) => {
+  if (item.dataset.disabled) {
+    item.classList.add("container-slider_disabled");
+    inputs[i].disabled = "true";
+  }
+})
